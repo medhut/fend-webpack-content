d2036e70b135a7af5d212c6b3e576062b87a549d
Medhat <medhut@yahoo.com>
2021-03-06 16:43:39 +0200

    Installed Html webpack Plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 const path = require("path")
 const webpack = require("webpack")
 module.exports = {
     entry: './src/client/index.js',
+    mode: 'development',
     module: {
         rules: [
                 {
@@ -11,5 +13,10 @@ module.exports = {
                 }
         ]
 },
-    mode: 'development'
+plugins:[
+    new HtmlWebpackPlugin({
+        template: "./src/client/views/index.html",
+        filename: "./index.html",
+    })
+]
 }
